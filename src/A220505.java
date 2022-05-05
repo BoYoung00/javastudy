7ab49d5ca086229b691fe2963a52e9ad6f1bc81c
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-05-06 00:07:53 +0900

    Helloworld

---
+++ src/A220505.java
@@ -0,0 +1,28 @@
+public class A220505 {
+    public static void main(String[]args) {
+        a();
+        b();
+        c();
+        d();
+    }
+    public static void a() {
+        int a = 5, b = 5;
+        System.out.println(a + b);
+    }
+
+    public static void b() {
+        int a = 5, b = 5;
+        System.out.println(a - b);
+    }
+
+    public static void c() {
+        int a = 5, b = 5;
+        System.out.println(a * b);
+    }
+
+    public static void d() {
+        int a = 5, b = 5;
+        System.out.println(a / b);
+    }
+
+}
---
+++ src/Helloworld.java
@@ -0,0 +1,20 @@
+import java.security.spec.RSAOtherPrimeInfo;
+public class Helloworld {
+    public static void main(String[]args) {
+        Hello();
+
+    }
+    public static void Hello() {
+        System.out.println("Hello");
+    }
+}
+//자바를 컴퓨터에서 돌리려면 JDK가 필요하다
+//자바는 객체지향 언어 중 하나다. 객체지향 여러부품들을 만들어서 그걸 불러내서 만든 것
+//기본적인 출력 3가지 = println printf print
+//변수 형 지정 (int a = 1)
+//문자열 변환 int a = Interger.paseint(b);
+// %나머지 /몫
+
+
+//변수 소문자로
+//class 메소드 대문자로 사용
