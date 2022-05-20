cd6dc01b25bab0feda26a936b9a238daafac4762
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2022-05-20 15:48:08 +0900

    Helloworld

---
+++ src/A220520.java
@@ -0,0 +1,74 @@
+public class A220520 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+//        double [] a = {3.56, 2.47, 3.56};
+//
+//        System.out.println("1학년 " + a[0]);
+//        System.out.println("2학년 " + a[1]);
+//        System.out.println("3학년 " + a[2]);
+
+//        int [] a = new int[5];
+//        a[0]=10;
+//        a[1]=20;
+//        a[2]=30;
+//        a[3]=40;
+//        a[4]=50;
+
+//        for (int i=0; i< a.length; i++) {
+//        System.out.println(a[i]); }
+
+
+//    int [] a = new int[10];
+//
+//        for (int i=0; i < a.length; i++) {
+//        a[i]=(int)Math.pow(2,i+1);
+//        System.out.println(i+1 +"번째 바이러스 증식 수 : "+ a[i]);
+//        }
+
+////객체 생성
+//        Shop s1= new Shop();
+//        s1.name="커피";
+//        s1.price=1000;
+//        s1.print();
+//
+//    }
+//}
+//
+//class Shop {
+//    //필드
+//    String name;
+//    int price;
+//
+//    //메소드
+//    void print() {
+//        System.out.println(name+" : "+price);
+//    }
+//}
+
+
+//    Character a1 = new Character();
+//
+//        a1.name ="ㅇㅇ";
+//                a1.hp=100;
+//                a1.punch="펀치";
+//
+//                a1.print();
+//
+//                }
+//                }
+//
+//class Character {
+//    String name;
+//    int hp;
+//
+//    String punch;
+//
+//    void print() {
+//        System.out.println(name + hp + punch);
+//    }
